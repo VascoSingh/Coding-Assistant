c224c7803e0fc58f21b79db10ec823af53cb9a2a
Vasco Singh <vasco@realignrx.com>
2023-01-03 12:58:23 -0800

    Fixed the prismJS languages

--- components/code.js
+++ components/code.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import Prism from "prismjs";
 import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-java"
+import "prismjs/components/prism-python"
 
 export default function Code({ code, language }) {
   useEffect(() => {
